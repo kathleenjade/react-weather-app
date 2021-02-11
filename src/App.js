9d2f5042aacc84209ec3de40b41cf8b5baeb1ca5
kathleenjade <73675192+kathleenjade@users.noreply.github.com>
2021-02-11 16:57:36 +0900

    updated styling in css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import './App.css';
-import Weather from './Weather'
+import Weather from './Weather';
+
 
 function App() {
-  return (
-    <div className="App">
-      <div className="container cool">
-      <Weather defaultCity="London" />
+  if (Weather.temperature === undefined || Weather.temperature < 16)  {
+    return (
+      <div className="App">
+        <div className="container cool">
+        <Weather defaultCity="London" />
+        </div>
+        <h2> This was coded by Kath Escote and is {" "}
+          <a href="https://github.com/kathleenjade/react-weather-app" target="_blank" rel="noreferrer">
+          open-sourced on Github.
+          </a>
+        </h2>
       </div>
-      <h2> This was coded by Kath Escote and is {" "}
-        <a href="https://github.com/kathleenjade/react-weather-app" target="_blank" rel="noreferrer">
-        open-sourced on Github.
-        </a>
-      </h2>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="App">
+        <div className="container">
+        <Weather defaultCity="London" />
+        </div>
+        <h2> This was coded by Kath Escote and is {" "}
+          <a href="https://github.com/kathleenjade/react-weather-app" target="_blank" rel="noreferrer">
+          open-sourced on Github.
+          </a>
+        </h2>
+      </div>
+    );
+  }
 }
+
+
+
 
 export default App;
